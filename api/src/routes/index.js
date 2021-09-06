5467d8dd512a151ecbd36c2222a7dc3139496550
franmassello <franmassello@gmail.com>
2021-09-06 12:01:47 -0300

    Add initial files frontend

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -10,8 +10,6 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-
-
 const getApiInfo = async()=>{
     const apiUrl = await axios.get('https://api.thedogapi.com/v1/breeds')
     const apiInfo = await apiUrl.data.map(el => {
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,13 +3,15 @@ import { useState, useEffect } from 'react';
 import { useDispatch, useSelector} from 'react-redux';
 import { getDogs } from '../actions';
 import { Link } from 'react-router-dom';
+import Dog from './Dog'
+
 
 export default function Home(){
     const dispatch = useDispatch()
-    const allDogs = useSelector((state) => state.dogs)
+    let allDogs = useSelector((state) => state.dogs.data)
     useEffect(()=>{
         dispatch(getDogs())
-    },[])
+    },[dispatch])
 
     function handleClick(e){
         e.preventDefault()
@@ -19,13 +21,14 @@ export default function Home(){
 return(
     <div>
         <Link to='/dogs'>Crear Perro</Link>
+        <h1>TestApp</h1>
         <button onClick={e => {handleClick(e)}}>
             Cargar los personajes
         </button>
         <div>
             <select>
-                <option value= 'asc'>Ascendente</option>
-                <option value= 'desc'>Descendente</option>
+                <option value= 'temp'>Temperamento</option>
+                <option value= 'raza'>Raza</option>
             </select>
 
             <select>
@@ -34,10 +37,23 @@ return(
             </select>
                 
             <select>
-                <option value= 'asc'>Ascendente</option>
-                <option value= 'desc'>Descendente</option>
+                <option value= 'orden'>Orden alfabetico</option>
+                <option value= 'peso'>Peso</option>
             </select>
+            
+            {allDogs?.map((c) =>{
+                return (
+                    <fragment>
+                        <Link to={'/home/'+c.id}>
+                            <Dog name={c.name} image={c.image} breed={c.breed} key={c.id} />
+                        </Link>
+                    </fragment>
+                )
+            })
+
+            }
+
         </div>
     </div>
-)
+    )
 }
