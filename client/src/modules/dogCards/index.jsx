6df8bcda5b2c97b28ef6bbd5d38be7a136474e03
FranMassello <frammassello1@gmail.com>
2021-09-06 23:23:30 -0300

    Add first part frontend

---
+++ client/src/modules/dogCards/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+//import { DOGS_URL } from "../../constants"
+import { getDogs } from "../../store/actions/dogActions";
+
+export default function DogCards() {
+  const dispatch = useDispatch();
+  const allDogs = useSelector((state) => state.dogs);
+
+  useEffect(() => {
+    dispatch(getDogs());
+  }, []);
+
+  return (
+    <div>
+      {allDogs?.map((dog) => {
+        return (
+          <div>
+            <p>{dog.name}</p>
+            <img src={dog.img.url} alt="IMG NOT FOUND" />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware } from "redux";
-import {composeWithDevTools} from 'redux-devtools-extension' 
-import thunk from 'redux-thunk'
-import rootReducer from "../reducer";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import reducer from './reducers/index'
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+
+export const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+
