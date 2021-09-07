ddf80243a17ab0972dcb940be18eab63d33cad2d
FranMassello <frammassello1@gmail.com>
2021-09-07 12:18:31 -0300

    Add basic frontend working

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
-import reducer from './reducers/index'
+import reducer from '../reducers/index'
 
 
 export const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
