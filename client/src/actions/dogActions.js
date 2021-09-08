37c82db094de2e2d01b2045dc3e5e2a878202601
franmassello <franmassello@gmail.com>
2021-09-08 14:39:58 -0300

    Add frontend stuff

--- client/src/actions/dogActions.js
+++ client/src/actions/dogActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { DOGS_URL } from '../constants'
 export const GET_DOGS = 'GET_ DOGS'
+export const GET_DOGS_BREED = 'GET_DOGS_BREED'
 
 export function getDogs(){
     return async (dispatch) => {
@@ -10,5 +11,16 @@ export function getDogs(){
            payload: allDOGS.data
        } 
        return dispatch(action)     
+    }
+}
+
+export function getDogsBreed(input){
+    return async (dispatch) => {
+        let dogsObtained = await axios(DOGS_URL)
+        let action = {
+            type: GET_DOGS_BREED,
+            payload: dogsObtained.data
+        }
+        return dispatch(action)
     }
 }
