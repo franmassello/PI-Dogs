119945648e53fc572b7875a182ae30b2071de006
FranMassello <frammassello1@gmail.com>
2021-09-09 17:00:28 -0300

    Add first part of asc desc sort

--- client/src/actions/dogActions.js
+++ client/src/actions/dogActions.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
-import { DOGS_URL } from '../constants'
+import { DOGS_URL, GETDOGS_URL } from '../constants'
 export const GET_DOGS = 'GET_ DOGS'
 export const GET_DOGS_BREED = 'GET_DOGS_BREED'
+export const SORT_BREEDS = 'SORT_BREEDS'
+
+function setFirstLetterToCap(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 export function getDogs(){
     return async (dispatch) => {
@@ -14,13 +19,96 @@ export function getDogs(){
     }
 }
 
-export function getDogsBreed(input){
-    return async (dispatch) => {
-        let dogsObtained = await axios(DOGS_URL)
-        let action = {
-            type: GET_DOGS_BREED,
-            payload: dogsObtained.data
+export function getDogsBreed(name){
+    return async function (dispatch){
+        try {
+            var json = await axios.get(GETDOGS_URL+name)
+            return dispatch({
+                type: GET_DOGS_BREED,
+                payload: json.data
+            })
+        } catch (error) {
+            console.log(error)
         }
-        return dispatch(action)
     }
 }
+export function sortBreeds(orden, dogs){
+    return async function (dispatch){
+       /*  console.log('Orden:',orden)
+        console.log('dogs:', dogs) */
+        let dogsSort = [...dogs]
+        //let dogsSortedArray = []
+        //console.log(dogsSort.length)
+
+        for(var i=0; i < dogsSort.length; i++){
+            var lowerCasedA = dogsSort[i].name.toLowerCase()  // Le hago lowercase para la comparacion
+            var lowerCasedB = dogsSort[i+1]?.name.toLowerCase()
+            console.log(lowerCasedA,'vs' ,lowerCasedB)
+            if(orden === 'asc'){
+                if(lowerCasedB){ //Verificacion de que exista B
+                    console.log('entro al primer if')
+                    if(lowerCasedA > lowerCasedB){
+                        console.log('entro al segundo if')
+                        console.log(lowerCasedA, 'es menor que', lowerCasedB)
+                    }
+                    if(lowerCasedA < lowerCasedB){ 
+                        console.log('entro al tercer if')
+                        console.log(lowerCasedA, 'es mayor que', lowerCasedB)
+                    }
+                }
+            }
+            if(orden === 'desc'){
+                if(lowerCasedB){ //Verificacion de que exista B
+                    console.log('entro al primer if DESC')
+                    if(lowerCasedA < lowerCasedB){
+                        console.log('entro al segundo if DESC')
+                        console.log(lowerCasedA, 'es menor que', lowerCasedB)
+                    }
+                    if(lowerCasedA > lowerCasedB){ 
+                        console.log('entro al tercer if DESC')
+                        console.log(lowerCasedA, 'es mayor que', lowerCasedB)
+                    }
+                }
+            }
+
+            //var upperCased = setFirstLetterToCap(dogsSort[i].name) // Le pongo la primer letra en uppercase para devolver
+            
+        }
+
+
+
+        /* dogsSort.sort(function(a,b){
+            var letA = a.name.toLowerCase()
+            var letB = b.name.toLowerCase()
+            //console.log(letA)
+            // LOGICA : CREAR ARRAY DE PERROS ORDENADOS Y DEVOLVER ESE ARRAY REMPLAZANDO EL ACTUAL
+            //console.log(letB)
+            if(orden === 'asc'){
+                if(letA < letB){
+                    //console.log(letB,'es mayor que', letA)
+                } // -1
+                if(letA > letB){
+                    //console.log(letB,'es menor que', letA)
+                } // 1
+                return 0
+            }
+
+            if(orden === 'desc'){
+                if(letA < letB){
+                    //console.log(letA,'es mayor que', letB)
+                } // 1
+                if(letA > letB){
+                    //console.log(letA,'es menor que', letB)
+                } // -1
+                return 0
+            }
+        }) */
+        return function(dispatch){
+            dispatch({
+                type: SORT_BREEDS,
+                payload: dogsSort
+            })
+        }
+    }
+}
+
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-//import { DOGS_URL } from "../../constants"
-import { getDogs, getDogsBreed } from "../../actions/dogActions";
+import { getDogs, getDogsBreed, sortBreeds } from "../../actions/dogActions";
 import Paginado from "../Paginado/Paginado";
+import SearchBar from "../SearchBar/SearchBar";
 
 export default function Home() {
     const dispatch = useDispatch();
@@ -15,6 +15,8 @@ export default function Home() {
     const indexOfFirstDog = indexOfLastDog - dogsPerPage
     const currentDogs = allDogs.slice(indexOfFirstDog, indexOfLastDog)
 
+    const [ searchTerm, setSearchTerm] = useState('')
+
     const paginado = (pageNumber) => {
         setCurrentPage(pageNumber)
     }
@@ -22,58 +24,71 @@ export default function Home() {
     useEffect(() => {
         dispatch(getDogs());
     }, []);
+    
 
     function handleClick(e) {
         e.preventDefault();
         dispatch(getDogs());
     }
 
-    function handleFilterBreed(e){
-        //console.log('HOla')
+    function handleChangeAscDesc(e){
+        e.preventDefault()
+        let valorOrden = e.target.value
+
+        dispatch(sortBreeds(valorOrden, allDogs))
+        //console.log(dispatch(sortBreeds(e.target.value, allDogs)))
+        //console.log(e.target.value) 
+        //console.log(allDogs) // es el arreglo de todos los perros
+        //console.log(allDogs[1].name) // Afhgan hound
+    }
+  /*   function handleFilterBreed(e){
         dispatch(getDogsBreed(e.target.value))
     }
-    
+     */
     return (
         <div>
         <Link to="/dogs">Crear Perro</Link>
         <h1>DogsApp</h1>
         <button onClick={(e) => {handleClick(e);}}>
-            Cargar los personajes
+            Recargar los personajes
         </button>
+
         <div>
-            <form className="breedSearch" onSubmit={handleFilterBreed}>
-                <label>Buscar Raza:   </label>
-                <input type="text" name="raza"></input>
-                <button>Enter</button> 
-                <br></br>
-            </form>
 
             <select>
             <option value="asc">Temperamento</option>
             </select>
 
-            <select>
-            <option value="asc">Ascendente</option>
-            <option value="desc">Descendente</option>
+            <select onChange={(e)=>{handleChangeAscDesc(e)}}>
+                <option value="asc">Ascendente</option>
+                <option value="desc">Descendente</option>
             </select>
 
             <select>
-            <option value="orden">Orden alfabetico</option>
             <option value="peso">Peso</option>
             </select>
             
             <div>
+            <SearchBar/>
             <Paginado
             dogsPerPage= {dogsPerPage}
             allDogs = {allDogs.length}
             paginado = {paginado}
             />
             
-            {currentDogs?.map((dog) => { // antes estaba alldogs.map
+            
+            {currentDogs.filter((dog) => {
+                if(searchTerm == ''){
+                    return dog
+                } else if (dog.name.toLowerCase().includes(searchTerm.toLowerCase())){
+                    return dog
+                }
+            }
+                ).map((dog) => { // antes estaba alldogs.map
                 return (
                 <div>
                     <p>Raza: {dog.name}</p>
-                    <p>Temperamentos: {dog.temperament.map((temp)=>{return temp + ' '})}</p>    
+                    <p>Temperamentos: {dog.temperament?.map((temp)=>{return temp + ' '})}</p>    
                     <p>Peso: {dog.weight}</p>
                     <img
                         src={dog.img.url}
@@ -85,11 +100,6 @@ export default function Home() {
                 );
             })}
             </div>
-            <Paginado
-            dogsPerPage= {dogsPerPage}
-            allDogs = {allDogs.length}
-            paginado = {paginado}
-            />
         </div>
         </div>
     );
