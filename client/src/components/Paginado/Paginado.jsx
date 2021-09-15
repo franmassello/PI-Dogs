efcf7feb4cd1b60aff3dee55a94bbd89055b2a13
FranMassello <frammassello1@gmail.com>
2021-09-15 17:11:19 -0300

    Add solved bugs

--- client/src/components/Paginado/Paginado.jsx
+++ client/src/components/Paginado/Paginado.jsx
@@ -9,7 +9,7 @@ export default function Paginado({dogsPerPage, allDogs, paginado}){
 
     return(
         <nav>
-            <ul className='pagination'>
+            <ul key='pagination' className='pagination'>
                 { pageNumbers &&
                 pageNumbers.map(number =>(
                     <li key={number}>
